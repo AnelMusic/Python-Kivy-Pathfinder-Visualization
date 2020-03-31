43fc339d53f844139b214dfbbdd10a6b2d93b994
Anel Music <32487291+AnelMusic@users.noreply.github.com>
2020-03-31 16:27:02 +0200

    Updated config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
+# Number of obstacles
+MAX_WALLELEMENTS = 5 
 
-MAX_WALLELEMENTS = 10 #50
-
+# Numeric algorithm naming
 ALGORITHM_ASTAR = 0
 ALGORITHM_DIJKSTRA = 1
 ALGORITHM_BFS = 2
@@ -9,9 +10,7 @@ ALGORITHM_GBFS = 4
 ALGORITHM_BIDIRECTIONAL_DIJKSTRA = 5
 ALGORITHM_ASTAR_8FIELD = 6
 
-
-
-
+# UI color properties
 node_color = [280/255, 280/255, 280/ 225, 0.7]
 btn_color = [280/255, 280/255, 280/ 225, 1]
 btn_clicked_color= [1,1,1,1]
@@ -20,6 +19,7 @@ final_path_color = [2, 2, 2, 0.95]
 targetnode_color = [10, 10, 0, 0.85]
 startnode_color = [0, 8.5, 8.5, 0.65]
 
+# Expansion rate for exploration and path planning
 update_path_rate = 0.1
 update_exploration_rate = 0.01
 
